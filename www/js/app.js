8d2dbbd6c28e04868461c743bdbc3a8a51efdc8b
Stefano Rainieri <stefano.rainieri@fazland.com>
2017-06-09 13:02:52 +0200

    delete element. included lodash.

--- www/js/app.js
+++ www/js/app.js
@@ -1,149 +1,209 @@
-// Ionic Starter App
-
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'my-formulas' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('my-formulas', ['ionic'])
 
-// This factory allow to load and save formulas from / to local storage
-.factory('Formulas', function () {
-  return {
+    // This factory allow to load and save formulas
+    // from / to local storage
+    .factory('Formulas', function () {
+        return {
 
-    all: function () {
-      var formulas = window.localStorage['formulas'];
-      return formulas ? angular.fromJson(formulas) : [];
-    },
+            /**
+             * @description Get all formulas on local storage
+             * @return {*}
+             */
+            all: function () {
+                var formulas = window.localStorage['formulas'];
+                return formulas ? angular.fromJson(formulas) : [];
+            },
 
-    save: function(formulas) {
-      window.localStorage['formulas'] = angular.toJson(formulas);
-    },
+            /**
+             * @description Save all formulas
+             * @param formulas
+             */
+            save: function (formulas) {
+                window.localStorage['formulas'] = angular.toJson(formulas);
+            },
 
-    create: function (title) {
-      return {
-        title: title,
-        elements: []
-      }
-    },
+            /**
+             * @description Create formula object
+             * @param title
+             * @return {{title: *, elements: Array}}
+             */
+            create: function (title) {
+                return {
+                    title: title,
+                    elements: []
+                }
+            },
 
-    getLastActiveIndex: function () {
-      return parseInt(window.localStorage['lastActiveFormula']) || 0;
-    },
+            /**
+             * @description Get the index of last active formula object
+             * @return {Number|number}
+             */
+            getLastActiveIndex: function () {
+                return parseInt(window.localStorage['lastActiveFormula']) || 0;
+            },
 
-    setLastActiveIndex: function (index) {
-      window.localStorage['lastActiveFormula'] = index;
-    }
-  }
-})
+            /**
+             * @description Set the index of last active formula object
+             * @param index
+             */
+            setLastActiveIndex: function (index) {
+                window.localStorage['lastActiveFormula'] = index;
+            }
+        }
+    })
 
-// This controller allow to add and remove elements to a specific formula
-// and manage formulas with the proper factory
-.controller('ElementsCtrl', function ($scope, $timeout, $ionicModal, Formulas, $ionicSideMenuDelegate) {
+    // This controller allow to add and remove elements to a specific formula
+    // and manage formulas with the proper factory
+    .controller('ElementsCtrl', function ($scope, $timeout, $ionicModal, Formulas, $ionicSideMenuDelegate) {
 
-  var _createFormula = function (title) {
-    var formula = Formulas.create(title);
-    $scope.formulas.push(formula);
+        /**
+         * @param title
+         * @private
+         */
+        var _createFormula = function (title) {
+            var formula = Formulas.create(title);
+            $scope.formulas.push(formula);
 
-    Formulas.save($scope.formulas);
-    $scope.selectFormula(formula, $scope.formulas.length - 1);
-  };
+            Formulas.save($scope.formulas);
+            $scope.selectFormula(formula, $scope.formulas.length - 1);
+        };
 
-  // Load / initialize formulas
-  $scope.formulas = Formulas.all();
+        /**
+         * @description Load / initialize formulas
+         */
+        $scope.formulas = Formulas.all();
 
-  // Set the active formula
-  $scope.activeFormula = $scope.formulas[Formulas.getLastActiveIndex()];
+        /**
+         * @description Set the active formula
+         */
+        $scope.activeFormula = $scope.formulas[Formulas.getLastActiveIndex()];
 
-  // Create a formula
-  $scope.newFormula = function () {
-    console.log('annannata');
-    var title = prompt('Your brand new secret formula is: ');
+        /**
+         * @description Create a formula
+         */
+        $scope.newFormula = function () {
+            console.log('annannata');
+            var title = prompt('Your brand new secret formula is: ');
 
-    if (title) {
-      _createFormula(title);
-    }
-  };
+            if (title) {
+                _createFormula(title);
+            }
+        };
 
-  // Choose a formula and close left menu
-  $scope.selectFormula = function (formula, index) {
-    $scope.activeFormula = formula;
-    Formulas.setLastActiveIndex(index);
+        /**
+         * @description Choose a formula and close left menu
+         * @param formula
+         * @param index
+         */
+        $scope.selectFormula = function (formula, index) {
+            $scope.activeFormula = formula;
+            Formulas.setLastActiveIndex(index);
 
-    $ionicSideMenuDelegate.toggleLeft(false);
-  };
+            $ionicSideMenuDelegate.toggleLeft(false);
+        };
 
-  // Load modal new-element
-  $ionicModal.fromTemplateUrl('new-element.html', function (modal) {
-      $scope.elementModal = modal;
-    }, {
-      scope: $scope
-  });
+        /**
+         * @description Load modal new-element
+         */
+        $ionicModal.fromTemplateUrl('new-element.html', function (modal) {
+            $scope.elementModal = modal;
+        }, {
+            scope: $scope
+        });
 
-  // Submit form
-  $scope.addElement = function (element) {
-    if (!$scope.activeFormula || !element) {
-      return;
-    }
+        /**
+         * @description Submit form
+         * @param element
+         */
+        $scope.addElement = function (element) {
+            if (!$scope.activeFormula || !element) {
+                return;
+            }
 
-    $scope.activeFormula.elements.push({
-      name: element.name,
-      color: element.color,
-      qty: element.qty
+            $scope.activeFormula.elements.push({
+                id: new Date().valueOf(),
+                name: element.name,
+                color: element.color,
+                qty: element.qty
+            });
+
+            $scope.elementModal.hide();
+            Formulas.save($scope.formulas);
+
+            element.name = '';
+            element.color = 'stable';
+            element.qty = null;
+        };
+
+        $scope.deleteElement = function (id) {
+            if (!$scope.activeFormula) {
+                return;
+            }
+
+            $scope.activeFormula.elements = _.reject($scope.activeFormula.elements, function (element) {
+                return element.id === id;
+            });
+
+            Formulas.save($scope.formulas);
+        };
+
+        /**
+         * @description Show modal
+         */
+        $scope.newElement = function () {
+            $scope.elementModal.show();
+        };
+
+        /**
+         * @description Hide modal
+         */
+        $scope.closeNewElement = function () {
+            $scope.elementModal.hide();
+        };
+
+        /**
+         * @description Open left menu
+         */
+        $scope.toggleFormulas = function () {
+            $ionicSideMenuDelegate.toggleLeft();
+        };
+
+        /**
+         * @description Purpose an unlimited timeout prompt to add the first formula if there's no one
+         */
+        $timeout(function () {
+            if (0 < $scope.formulas.length) {
+                return;
+            }
+
+            var title = null;
+            while (!title) {
+                title = prompt('Your FIRST brand new secret formula is: ');
+
+                if (title) {
+                    _createFormula(title);
+                }
+            }
+        }, 1000);
+    })
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
     });
-
-    $scope.elementModal.hide();
-    Formulas.save($scope.formulas);
-
-    element.name = '';
-    element.color = 'stable';
-    element.qty = null;
-  };
-
-  // Show modal
-  $scope.newElement = function () {
-    $scope.elementModal.show();
-  };
-
-  // Hide modal
-  $scope.closeNewElement = function () {
-    $scope.elementModal.hide();
-  };
-
-  // Open left menu
-  $scope.toggleFormulas = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-
-  // Purpose an unlimited timeout prompt to add the first formula if there's no one.
-  $timeout(function () {
-    if (0 < $scope.formulas.length) {
-      return;
-    }
-
-    var title = null;
-    while (!title) {
-      title = prompt('Your FIRST brand new secret formula is: ');
-
-      if (title) {
-        _createFormula(title);
-      }
-    }
-  }, 1000)
-})
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-});
